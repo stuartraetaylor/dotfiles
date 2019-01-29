2c2e5f2567b098b26b516a98e5477e95ede6b1fb
Stuart Taylor <stuartraetaylor@users.noreply.github.com>
2019-01-29 10:30:11 +0000

    Update install script

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ declare -a dotfiles=("bashrc" "vimrc" "gitconfig" "tmux.conf")
 
 for f in "${dotfiles[@]}"; do
     echo
-    read -r -p "Update .$f (overwrite existing file): [Y]es, [N]o? " r   
-    if [[ $r =~ ^([yY])$ ]]; then
-        echo "Updating .$f ..."
+    read -r -p "Install .$f (overwrite existing file) [Y/n]? " r   
+    if [[ $r =~ ^(\s*|[yY])$ ]]; then
+        echo "Creating symlink ~/.$f ..."
         ln -fs $PWD/$f $HOME/.$f
     else
         echo "Skipping .$f ..."
