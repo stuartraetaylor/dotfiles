5261e3a1b66c440f253e16008ccbd479c0dde597
Stuart Taylor <stuartraetaylor@users.noreply.github.com>
2019-01-29 16:17:10 +0000

    Whitespace.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ declare -a dotfiles=("bashrc" "vimrc" "gitconfig" "tmux.conf")
 
 for f in "${dotfiles[@]}"; do
     echo
-    read -r -p "Install .$f (overwrite existing file) [Y/n]? " r   
+    read -r -p "Install .$f (overwrite existing file) [Y/n]? " r
     if [[ $r =~ ^(\s*|[yY])$ ]]; then
         echo "Creating symlink ~/.$f ..."
         ln -fs $PWD/$f $HOME/.$f
