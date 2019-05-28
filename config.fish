47b858aa5d9988729b960dc81c878c2c31f1f15b
Stuart Taylor <stuartraetaylor@users.noreply.github.com>
2019-05-28 17:46:27 +0100

    Add fish config

---
+++ config.fish
@@ -0,0 +1,24 @@
+# Colour aliases.
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+# some more aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias ta='tmux attach'
+
+alias bashrc='source ~/.bashrc'
+
+alias gw='./gradlew'
+
+alias gs='git status'
+alias gl='git log'
+alias gd='git diff'
+alias gf='git fetch'
+alias gp='git pull'
+
+alias vir='vim -M'
+
